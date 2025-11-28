0509697e9d735e4e6652c54ff9264fbe5e529c14
Bruno Pequeno <bruno.pequeno@loyaltycom.com.br>
2025-11-28 12:25:26 -0300

    feat: :zap: Adicionando anotação

--- src/main/java/loyaltycom/api_selos/ApiSelosApplication.java
+++ src/main/java/loyaltycom/api_selos/ApiSelosApplication.java
@@ -3,9 +3,13 @@ package loyaltycom.api_selos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @ComponentScan
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ApiSelosApplication {
 
 	public static void main(String[] args) {
